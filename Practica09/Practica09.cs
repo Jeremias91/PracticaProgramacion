16a4ae897372cf6f0408e29ec72b4655b918f0b3
Jeremías Vergara <126523676+Jeremias91@users.noreply.github.com>
2024-09-03 15:31:42 -0300

    Add files via upload

---
+++ Practica09/Practica09.cs
@@ -0,0 +1,17 @@
+//67
+using System;
+
+class Practica09{
+    static void Main()
+    {
+        int numero = 1;
+
+        do{
+            Console.WriteLine(numero);
+            numero++;
+
+        }while(numero !=11);
+
+
+    }
+}
