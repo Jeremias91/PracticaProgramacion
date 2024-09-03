5db0827e5e21d260e6c811f56174d2af26741998
Jeremías Vergara <126523676+Jeremias91@users.noreply.github.com>
2024-09-03 15:32:27 -0300

    Add files via upload

---
+++ Practica10/Practica10.cs
@@ -0,0 +1,20 @@
+//68
+using System;
+
+class Practica10{
+
+    static void Main(){
+
+        int numero = 26;
+
+        
+            while( numero >= 10)
+            {
+           
+            Console.WriteLine(numero);
+            numero-= 2;
+
+            }
+    }
+
+}
